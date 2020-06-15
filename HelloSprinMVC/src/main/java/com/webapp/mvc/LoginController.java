5bd4d7e6074746d5429c78d69a483d8c3243d7a8
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-06-15 19:04:57 +0300

    added omers name

--- HelloSprinMVC/src/main/java/com/webapp/mvc/LoginController.java
+++ HelloSprinMVC/src/main/java/com/webapp/mvc/LoginController.java
@@ -3,6 +3,12 @@ package com.webapp.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+ * 
+ * author omer samancioglu
+ * 
+ */
+
 @Controller
 public class LoginController {
 	
