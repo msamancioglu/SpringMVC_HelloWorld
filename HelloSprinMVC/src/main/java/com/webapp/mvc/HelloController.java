9b1ae4b48ca94ad6163d7b92d1620393c40f3bae
Mustafa Samancioglu <msamancioglu@hotmail.com>
2020-06-15 16:00:45 +0300

    delete author details

--- HelloSprinMVC/src/main/java/com/webapp/mvc/HelloController.java
+++ HelloSprinMVC/src/main/java/com/webapp/mvc/HelloController.java
@@ -5,13 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-/****
- * 
- * @author Mustafa Samancioglu
- * 
- * reach me at msamancioglu@hotmail.com 
- *
- */
 @Controller
 public class HelloController {
 	@RequestMapping(value = "/hello")
